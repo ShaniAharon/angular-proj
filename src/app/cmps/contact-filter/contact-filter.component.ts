503b3ad7e2b9e54f2a0592bdac23db0bbd55c85d
shforeal <rivena14798@gmail.com>
2022-08-20 19:47:58 +0300

    feat: filter flow

--- src/app/cmps/contact-filter/contact-filter.component.ts
+++ src/app/cmps/contact-filter/contact-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ContactFilter } from 'src/app/models/contact.filter';
 import { ContactService } from 'src/app/services/contact.service';
@@ -6,23 +6,25 @@ import { ContactService } from 'src/app/services/contact.service';
 @Component({
   selector: 'contact-filter',
   templateUrl: './contact-filter.component.html',
-  styleUrls: ['./contact-filter.component.scss']
+  styleUrls: ['./contact-filter.component.scss'],
 })
-export class ContactFilterComponent implements OnInit {
+export class ContactFilterComponent implements OnInit, OnDestroy {
+  filterBy: ContactFilter;
+  subscription: Subscription;
 
-  filterBy: ContactFilter
-  subscription: Subscription
-
-  constructor(private contactService: ContactService) { }
+  constructor(private contactService: ContactService) {}
 
   ngOnInit(): void {
-    this.subscription = this.contactService.filterBy$.subscribe(filterBy => {
-      this.filterBy = filterBy
-    })
+    this.subscription = this.contactService.filterBy$.subscribe((filterBy) => {
+      this.filterBy = filterBy;
+    });
   }
 
   onSetFilter() {
-    this.contactService.setFilter({...this.filterBy})
+    this.contactService.setFilter({ ...this.filterBy });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
